f67a4ca8f0751f0422fdb4e2847d9dd461ff71ed
aaboundi <aadja@gmail.com>
2022-09-12 15:06:53 -0700

    Update-task7

--- models/user.py
+++ models/user.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """Represents a user for a MySQL database.
-    
+
     Inherits from SQLAlchemy Base and links to the MySQL table users.
     Attributes:
         __tablename__ (str): The name of the MySQL table to store users.
