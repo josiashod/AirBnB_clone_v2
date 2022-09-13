4e43cac98d2e8d86555628a58c86c2661f150470
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-13 23:00:54 +0000

    Task-6

--- models/city.py
+++ models/city.py
@@ -12,5 +12,6 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey(State.id))
-    name = Column(String(128))
+    state_id = Column(String(60), ForeignKey("states.id"))
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
