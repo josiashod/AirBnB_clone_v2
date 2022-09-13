ae3b28034a2fbe977c46ad9701a72aa634ff9046
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-13 22:11:30 +0000

    Update

--- models/review.py
+++ models/review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+"""Defines the Review class."""
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
