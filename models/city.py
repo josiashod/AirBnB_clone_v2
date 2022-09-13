163f0948beec4170f814ec3ce7cf5bf46626256e
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-13 23:14:40 +0000

    attribute

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 import os
-from models.base_model import Base, BaseModel
+from models.base_model import BaseModel
+from models.base_model import Base
 from models.state import State
 from sqlalchemy import Column, String, ForeignKey
 
@@ -12,6 +13,6 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey("states.id"))
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
     places = relationship("Place", backref="cities", cascade="delete")
