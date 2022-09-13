d1912b9a10021c995ad1b9ba02624bca98fbfc5c
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-13 22:33:56 +0000

    ForeignKey

--- models/review.py
+++ models/review.py
@@ -19,5 +19,5 @@ class Review(BaseModel):
     """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    place_id = Column(String(60), ForeignKey(Place.id), nullable=False)
+    user_id = Column(String(60), ForeignKey(User.id), nullable=False)
