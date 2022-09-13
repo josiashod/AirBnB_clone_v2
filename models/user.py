8268e71502d5fcb3860e0015a9c67f2d0f968689
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-13 22:25:32 +0000

    Up_user

--- models/user.py
+++ models/user.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import os
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+
+HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
