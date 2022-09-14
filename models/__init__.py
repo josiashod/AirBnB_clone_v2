1ddb602ea0f6318113bc46499f89b3b633b68599
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-14 02:12:54 +0000

    up_reload

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,6 @@ HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
 if HBNB_TYPE_STORAGE == "db":
     storage = DBStorage()
-    storage.reload()
 else:
     storage = FileStorage()
-    storage.reload()
+storage.reload()
