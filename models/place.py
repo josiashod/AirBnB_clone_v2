b95e4fd221d25ad3b491d780e09eda61fed9dd36
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-14 00:49:23 +0000

    up_FK

--- models/place.py
+++ models/place.py
@@ -20,10 +20,10 @@ HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
 
 association_table = Table("place_amenity", Base.metadata,
                           Column("place_id", String(60),
-                                 ForeignKey("places.id"),
+                                 ForeignKey(places.id),
                                  primary_key=True, nullable=False),
                           Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
+                                 ForeignKey(amenities.id),
                                  primary_key=True, nullable=False))
 
 
@@ -47,8 +47,8 @@ class Place(BaseModel, Base):
         amenity_ids (list): An id list of all linked amenities.
     """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey(cities.id), nullable=False)
+    user_id = Column(String(60), ForeignKey(users.id), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
     number_rooms = Column(Integer, default=0)
